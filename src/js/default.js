5c5dfe1df5355b12c2266fab9b4e33eee5fb73f9
ajayiyer <iyer.aj@gmail.com>
2016-01-07 06:48:17 +0530

    added sticky nav bar

--- src/js/default.js
+++ src/js/default.js
@@ -2,18 +2,30 @@
 
 // Setup WOW.js
 var wow = new WOW({
-  boxClass:     'content-block',
-  animateClass: 'active',
-  offset:       1,
-  mobile:       true,
-  live:         true
+    boxClass: 'content-block',
+    animateClass: 'active',
+    offset: 1,
+    mobile: true,
+    live: true
 });
 // Initiate WOW.js
 wow.init();
 
 // Load Custom Google Font
 WebFont.load({
-  google: {
-    families: ['Open Sans:400,700', 'Yeseva One:400']
-  }
+    google: {
+        families: ['Open Sans:400,700', 'Yeseva One:400']
+    }
+});
+
+var navMenu = $("#navigation-menu");
+stickyCls = "navigation-sticky";
+hdr = $('.header').height();
+nm = $('#navigation-menu').height();
+$(window).scroll(function() {
+    if ($(this).scrollTop() > (hdr-nm)) {
+        navMenu.addClass(stickyCls);
+    } else {
+        navMenu.removeClass(stickyCls);
+    }
 });
